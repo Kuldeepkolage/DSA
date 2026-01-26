2b4853e9404ed57f39bcca512c4aac076ce39c1a
Kuldeep Kishan Kolage <kolagekuldeep09@gmail.com>
2026-01-26 21:53:17 +0530

    Pattern 7 Solved

---
+++ Patterns/pat7.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number of rows: "))
+for i in range(0,n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(2*i+1):
+        print("*",end="")   
+    for l in range(n-i-1):
+            print(" ",end="")   
+    print()
