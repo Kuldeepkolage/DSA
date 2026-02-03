a87f4907f4eecbb97e1806df40a7c9b31bec8798
Kuldeep Kishan Kolage <kolagekuldeep09@gmail.com>
2026-02-03 09:54:44 +0530

    Pattern 12 Solved

--- Patterns/pat12.py
+++ Patterns/pat12.py
@@ -1,12 +1,37 @@
-n = int(input("Enter a number:"))
-for i in range(0,n):
-    for j in range(n-i-1):
-        print(" ", end=" ")
-    if i%2==0:
-        start = 1
-    else:
-        start = 0   
-    for j in range(i+1):
-        print(start, end=" ")
-        start = 1 - start
-    print()
+class Solution:
+    # Function to print pattern12
+    def pattern12(self, n):
+        # Initial no. of spaces in row 1.
+        spaces = 2 * (n - 1)
+
+        # Outer loop for the number of rows.
+        for i in range(1, n + 1):
+            # For printing numbers in each row
+            for j in range(1, i + 1):
+                print(j, end="")
+
+            # For printing spaces in each row
+            for j in range(1, spaces + 1):
+                print(" ", end="")
+
+            # For printing numbers in each row
+            for j in range(i, 0, -1):
+                print(j, end="")
+
+            """ As soon as the numbers for each iteration
+            are printed, we move to the next row and give
+            a line break otherwise all numbers would get 
+            printed in 1 line"""
+            print()
+
+            """ After each iteration nos. increase by 
+            2, thus spaces will decrement by 2"""
+            spaces -= 2
+
+if __name__ == "__main__":
+    N = 5
+
+    # Create an instance of Solution class
+    sol = Solution()
+
+    sol.pattern12(N)
