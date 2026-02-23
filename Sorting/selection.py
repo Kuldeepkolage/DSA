9329b477eee0825a05795ef64815814614bb52c4
Kuldeep Kishan Kolage <kolagekuldeep09@gmail.com>
2026-02-23 20:22:34 +0530

    Sorted the array using Selection Sort

---
+++ Sorting/selection.py
@@ -0,0 +1,17 @@
+n = int(input("Enter the number of elements: "))
+arr = [] 
+for i in range(n):
+    element = int(input("Enter element: "))
+    arr.append(element)
+
+for i in range(len(arr)-1):
+    min_index = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+print("Sorted array:", arr)
+
+
+# Time complexity: O(n^2)
+# Space complexity: O(1)    
