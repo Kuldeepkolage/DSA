32ca5e522017493dab0755dad40f801bca539085
Kuldeep Kolage <kuldeep.233234109@vcet.edu.in>
2026-07-29 22:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.47%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and not (n & (n - 1))
+        if n<=0:
+            return False
+        while n%2==0:
+            n//=2
+        return n==1
+        
+        
